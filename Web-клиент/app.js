24b8ba357c23962fccb5dc56914eb32a0fb68142
viivevv <kto.to.0000f@gmail.com>
2026-01-06 23:53:46 +0300

    Update app.js

--- Web-клиент/app.js
+++ Web-клиент/app.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Web Client модуль работает!</h1><p>Аутентификация и сессии в разработке...</p>');
+});
+
+app.listen(port, () => {
+  console.log(`Web Client запущен на http://localhost:${port}`);
+});
