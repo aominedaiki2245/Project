95c1b1088f40aa18a1ab03fd22ad53d536c00136
Elvis_234 <temnyjvlastelin996@gmail.com>
2026-01-08 08:44:28 +0300

    Create middleware.go

---
+++ МОДУЛЬ АВТОРИЗАЦИИ/internal/oauth/middleware.go
@@ -0,0 +1,26 @@
+package oauth
+
+import (
+	"net/http"
+	"strings"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			http.Error(w, "Нет токена", http.StatusUnauthorized)
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		claims, err := ValidateToken(tokenStr)
+		if err != nil {
+			http.Error(w, "Неверный токен", http.StatusUnauthorized)
+			return
+		}
+
+		r.Header.Set("X-User-ID", claims.Identifier)
+		next.ServeHTTP(w, r)
+	})
+}
